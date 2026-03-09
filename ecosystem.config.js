3194be19a1b8dab11a0cb576cd65d1a6e773bec9
RyanGaplek <92612190+RyanGaplek@users.noreply.github.com>
2026-03-09 14:37:41 +0700

    Refactor ecosystem.config.js structure

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,28 +20,9 @@ module.exports = {
       name: "Tuyul-3", script: "bot.py", interpreter: "python3",
       env: { BOT_NAME: "peaxel4", API_KEY: "mr_live_1cEZm2NYLdMK4Rx7YTvFwe6xuj7qaCvD" }
     },
-    { 
-      name: "Tuyul-3", script: "bot.py", interpreter: "python3",
-      env: { BOT_NAME: "peaxel5", API_KEY: "mr_live_qS9FQmELIfLOF8ZTxAQNh0EVOPTKkBta" }
-    },
-    { 
-      name: "Tuyul-3", script: "bot.py", interpreter: "python3",
-      env: { BOT_NAME: "peaxel6", API_KEY: "mr_live_xysPJejffFUcpkmC_XoizRe-nmi_9NMX" }
-    },
-    { 
-      name: "Tuyul-3", script: "bot.py", interpreter: "python3",
-      env: { BOT_NAME: "peaxel7", API_KEY: "mr_live_HIO4r0hedXU8f8RDZsHWjMwKDdBHRiMc" }
-    },
-    { 
-      name: "Tuyul-3", script: "bot.py", interpreter: "python3",
-      env: { BOT_NAME: "peaxel8", API_KEY: "mr_live_b-7oSSJMfkIQWwY_wgoJFGi4eiYGUvUb" }
-    },
-    { 
-      name: "Tuyul-3", script: "bot.py", interpreter: "python3",
-      env: { BOT_NAME: "peaxel9", API_KEY: "mr_live_ojydX_TbwQ45eMM5LoQqX7YMh9QKE2gb" }
-    },
     // ... DAN SETERUSNYA SAMPAI BOT KE-15 ...
     // Tinggal copy-paste blok di atas ke bawah
   ]
 
 };
+
